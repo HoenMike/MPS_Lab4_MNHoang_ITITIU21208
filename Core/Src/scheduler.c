d66599eee5f1d991bed9ac350b0c074fc64ae5e9
HoènMike <hoanggg003@gmail.com>
2024-05-19 20:10:56 +0700

    update code in scheduler

--- Core/Src/scheduler.c
+++ Core/Src/scheduler.c
@@ -18,13 +18,12 @@ void SCH_Add_Task(void (*pFunction)(), uint32_t DELAY, uint32_t PERIOD)
 {
   if (current_index_task < SCH_MAX_TASKS)
   {
-    SCH_tasks_G[current_index_task].pTask = pFunction;
-    SCH_tasks_G[current_index_task].Delay = DELAY;
-    SCH_tasks_G[current_index_task].Period = PERIOD;
-    SCH_tasks_G[current_index_task].RunMe = 0;
-
-    SCH_tasks_G[current_index_task].TaskID = current_index_task;
-
+    sTasks *task = &SCH_tasks_G[current_index_task];
+    task->pTask = pFunction;
+    task->Delay = DELAY;
+    task->Period = PERIOD;
+    task->RunMe = 0;
+    task->TaskID = current_index_task;
     current_index_task++;
   }
 }
